62dec8349940481b66383ed9d114abbd663e7254
Shiva Ghose <shivaghose@users.noreply.github.com>
2016-04-07 23:00:21 -0700

    More organization and tools!

--- fzf_ros.sh
+++ fzf_ros.sh
@@ -1,35 +1,71 @@
+#==============================================================================
+# General ros tools
+
+## roscd
 rcd() {
     local package
     package=$(rospack list-names | fzf-tmux --query="$1" --select-1 --exit-0) &&
         roscd "$package"
 }
 
+#==============================================================================
+# Topic related tools
+## Echo a topic
 rte() {
     rostopic list > /dev/null &&
         rostopic list | fzf-tmux --query="$1" --select-1 --exit-0 |\
         sed "s/^/rostopic echo /" | writecmd
 }
 
-rkill() {
+## Topic info
+rtinfo() {
+    local topic
+    rostopic list > /dev/null &&
+        topic=$(rostopic list | fzf-tmux --query="$1" --select-1 --exit-0) &&
+        rosnode info "$topic"
+}
+
+#==============================================================================
+# Node related tools
+## Node info
+rninfo() {
+    local node
+    rostopic list > /dev/null
+        node=$(rosnode list | fzf-tmux --query="$1" --select-1 --exit-0) &&
+        rosnode info "$node"
+}
+
+## Ping nodes
+rnping() {
+    local node
+    rostopic list > /dev/null &&
+        node=$(rosnode list | fzf-tmux --query="$1" --select-1 --exit-0) &&
+        rosnode ping "$node"
+}
+
+## Kill nodes
+rnkill() {
     local node
     rostopic list > /dev/null &&
         node=$(rosnode list | fzf-tmux --query="$1" --select-1 --exit-0) &&
         rosnode kill "$node"
 }
 
-# Build packages
+#==============================================================================
+# Build tools
+## Build pacakges
 rb() {
     local package
     package=$(rospack list-names | fzf-tmux --query="$1" --select-1 --exit-0) &&
         catkin build -w $ROS_DIR_PATH -DCMAKE_EXPORT_COMPILE_COMMANDS=ON "$package" 
 }
 
-# Clean packages
+## Clean packages
 rclean() {
     local package
     package=$(rospack list-names | fzf-tmux --query="$1" --select-1 --exit-0)
-    rclean_build_path=$ROS_DIR_PATH/build/$package
-    rclean_devel_path=$ROS_DIR_PATH/devel/lib/$package
+    local rclean_build_path=$ROS_DIR_PATH/build/$package
+    local rclean_devel_path=$ROS_DIR_PATH/devel/lib/$package
     if [ -d $rclean_build_path ]; then
         echo "Removing $rclean_build_path" &&
         rm -rf $rclean_build_path > /dev/null
